da14189b389e4a6afa44d1306f6f77cd62014890
Anthony Carrillo <anthonycarrilloch7@gmail.com>
2024-04-04 20:44:25 -0600

    cambios

---
+++ WebApplication1/WebForm1.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Web.UI;
+
+namespace AppInsegura
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            string connectionString = "Data Source=ANTHONY\\SQLEXPRESS;Initial Catalog=Asignacion3;User ID=sa;Password=Mecagoenrafa7";
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+
+            // Consulta insegura
+            string query = "SELECT * FROM Users WHERE UserName = '" + username + "' AND password = '" + password + "'";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+
+
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    // Autenticación exitosa
+                    lblMessage.Text = "Autenticacion exitosa";
+
+                }
+                else
+                {
+                    // Credenciales inválidas
+                    lblMessage.Text = "Usuario o contraseña incorrectos.";
+                    lblMessage.ForeColor = System.Drawing.Color.Red;
+                }
+
+                reader.Close();
+            }
+        }
+    }
+}
